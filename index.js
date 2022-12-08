e99598b7eeb0483b52e573fe7bd677ca30e7063e
LauricioX <developerwebmasterpro@gmail.com>
2022-12-07 21:56:43 -0300

    adicionando cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = require("./routes/routes");
 const app = express();
+const cors = require("cors");
 
 //url encoded
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//setando cors (bloqueia requisições externas)
+app.use(cors());
 
 //setando rotas
 app.use("/", router);
